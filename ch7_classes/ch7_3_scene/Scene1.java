08a70a8e75cb636ae94a99dd402b3ee25bd609b1
Steve Maddock <stevemaddock@users.noreply.github.com>
2025-09-08 14:31:00 +0100

    u

---
+++ ch7_classes/ch7_3_scene/Scene1.java
@@ -0,0 +1,116 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class Scene1 extends JFrame {
+  
+  private static final int WIDTH = 1024;
+  private static final int HEIGHT = 768;
+  private static final Dimension dimension = new Dimension(WIDTH, HEIGHT);
+  private GLCanvas canvas;
+  private Scene1_GLEventListener glEventListener;
+  private final FPSAnimator animator; 
+
+  public static void main(String[] args) {
+    Scene1 b1 = new Scene1("Scene1");
+    b1.getContentPane().setPreferredSize(dimension);
+    b1.pack();
+    b1.setVisible(true);
+    b1.canvas.requestFocusInWindow();
+  }
+
+  public Scene1(String textForTitleBar) {
+    super(textForTitleBar);
+    GLCapabilities glcapabilities = new GLCapabilities(GLProfile.get(GLProfile.GL3));
+    canvas = new GLCanvas(glcapabilities);
+    Camera camera = new Camera(Camera.DEFAULT_POSITION, Camera.DEFAULT_TARGET, Camera.DEFAULT_UP);
+    glEventListener = new Scene1_GLEventListener(camera);
+    canvas.addGLEventListener(glEventListener);
+    canvas.addMouseMotionListener(new MyMouseInput(camera));
+    canvas.addKeyListener(new MyKeyboardInput(camera));
+    getContentPane().add(canvas, BorderLayout.CENTER);
+    addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        animator.stop();
+        remove(canvas);
+        dispose();
+        System.exit(0);
+      }
+    });
+    animator = new FPSAnimator(canvas, 60);
+    animator.start();
+  }
+  
+}
+
+class MyKeyboardInput extends KeyAdapter  {
+  private Camera camera;
+  
+  public MyKeyboardInput(Camera camera) {
+    this.camera = camera;
+  }
+  
+  public void keyPressed(KeyEvent e) {
+    Camera.Movement m = Camera.Movement.NO_MOVEMENT;
+    if (e.getModifiersEx() == java.awt.event.InputEvent.SHIFT_DOWN_MASK)
+      switch (e.getKeyCode()) {
+        case KeyEvent.VK_W: m = Camera.Movement.FAST_FORWARD;  break;
+        case KeyEvent.VK_S: m = Camera.Movement.FAST_BACK;  break;
+        case KeyEvent.VK_A: m = Camera.Movement.FAST_LEFT;  break;
+        case KeyEvent.VK_D: m = Camera.Movement.FAST_RIGHT; break;
+        case KeyEvent.VK_Q: m = Camera.Movement.FAST_UP;  break;
+        case KeyEvent.VK_E: m = Camera.Movement.FAST_DOWN;  break;
+      }
+    else
+      switch (e.getKeyCode()) {
+        case KeyEvent.VK_W: m = Camera.Movement.FORWARD;  break;
+        case KeyEvent.VK_S: m = Camera.Movement.BACK;  break;
+        case KeyEvent.VK_A: m = Camera.Movement.LEFT;  break;
+        case KeyEvent.VK_D: m = Camera.Movement.RIGHT; break;
+        case KeyEvent.VK_Q: m = Camera.Movement.UP;  break;
+        case KeyEvent.VK_E: m = Camera.Movement.DOWN;  break;
+      }
+    camera.keyboardInput(m);
+  }
+}
+
+class MyMouseInput extends MouseMotionAdapter {
+  private Point lastpoint;
+  private Camera camera;
+  
+  public MyMouseInput(Camera camera) {
+    this.camera = camera;
+  }
+  
+    /**
+   * mouse is used to control camera position
+   *
+   * @param e  instance of MouseEvent
+   */    
+  public void mouseDragged(MouseEvent e) {
+    Point ms = e.getPoint();
+    float sensitivity = 0.001f;
+    float dx=(float) (ms.x-lastpoint.x)*sensitivity;
+    float dy=(float) (ms.y-lastpoint.y)*sensitivity;
+    //System.out.println("dy,dy: "+dx+","+dy);
+    // need to include shift key here as used as a modifier with key presses as well
+    int mask = MouseEvent.BUTTON1_DOWN_MASK & MouseEvent.SHIFT_DOWN_MASK;
+    if (e.getModifiersEx()==MouseEvent.BUTTON1_DOWN_MASK
+        || (e.getModifiersEx() & mask) == mask) {
+      camera.updateYawPitch(dx, -dy);
+    }
+    lastpoint = ms;
+  }
+
+  /**
+   * mouse is used to control camera position
+   *
+   * @param e  instance of MouseEvent
+   */  
+  public void mouseMoved(MouseEvent e) {   
+    lastpoint = e.getPoint(); 
+  }
+}
---
+++ ch7_classes/ch7_3_scene/Scene5_GLEventListener.java
@@ -0,0 +1,253 @@
+import gmaths.*;
+
+import java.nio.*;
+import com.jogamp.common.nio.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.util.*;
+import com.jogamp.opengl.util.awt.*;
+import com.jogamp.opengl.util.glsl.*;
+
+import com.jogamp.opengl.util.texture.*;
+  
+public class Scene5_GLEventListener implements GLEventListener {
+  
+  private static final boolean DISPLAY_SHADERS = false;
+  private Shader shaderCube, shaderLight;
+  private Camera camera;
+    
+  /* The constructor is not used to initialise anything */
+  public Scene5_GLEventListener(Camera camera) {
+    this.camera = camera;
+    this.camera.setPosition(new Vec3(4f,8f,15f));
+    this.camera.setTarget(new Vec3(0f,5f,0f));
+  }
+  
+  // ***************************************************
+  /*
+   * METHODS DEFINED BY GLEventListener
+   */
+
+  /* Initialisation */
+  public void init(GLAutoDrawable drawable) {
+    GL3 gl = drawable.getGL().getGL3();
+    System.err.println("Chosen GLCapabilities: " + drawable.getChosenGLCapabilities());
+    gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+    gl.glClearDepth(1.0f);
+    gl.glEnable(GL.GL_DEPTH_TEST);
+    gl.glDepthFunc(GL.GL_LESS);
+    gl.glFrontFace(GL.GL_CCW);    // default is 'CCW'
+    gl.glEnable(GL.GL_CULL_FACE); // default is 'not enabled'
+    gl.glCullFace(GL.GL_BACK);    // default is 'back', assuming CCW
+    initialise(gl);
+    startTime = getSeconds();
+  }
+  
+  /* Called to indicate the drawing surface has been moved and/or resized  */
+  public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    GL3 gl = drawable.getGL().getGL3();
+    gl.glViewport(x, y, width, height);
+    float aspect = (float)width/(float)height;
+    Mat4 p = Mat4Transform.perspective(45, aspect);
+    camera.setPerspectiveMatrix(p);
+  }
+
+  /* Draw */
+  public void display(GLAutoDrawable drawable) {
+    GL3 gl = drawable.getGL().getGL3();
+    render(gl);
+  }
+
+  /* Clean up memory, if necessary */
+  public void dispose(GLAutoDrawable drawable) {
+    // GL3 gl = drawable.getGL().getGL3();
+    // gl.glDeleteBuffers(1, vertexBufferId, 0);
+    // gl.glDeleteVertexArrays(1, vertexArrayId, 0);
+    // gl.glDeleteBuffers(1, elementBufferId, 0);
+  }
+
+  // ***************************************************
+  /* THE SCENE
+   * Now define all the methods to handle the scene.
+   * This will be added to in later examples.
+   */
+
+  private Model[] cube;
+  private Model[] flatPlane;
+  private Light light;
+  private Mat4[] roomTransforms;
+
+  // textures
+  private TextureLibrary textures;
+
+  public void initialise(GL3 gl) {
+    createRandomNumbers();
+
+    textures = new TextureLibrary();
+    textures.add(gl, "diffuse_container", "assets/textures/container2.jpg");
+    textures.add(gl, "specular_container", "assets/textures/container2_specular.jpg");
+    textures.add(gl, "chequerboard", "assets/textures/chequerboard.jpg");
+    textures.add(gl, "cloud", "assets/textures/cloud.jpg");
+    textures.add(gl, "matrix", "assets/textures/matrix.jpg");
+    textures.add(gl, "black1x1", "assets/textures/black1x1.jpg");
+    textures.add(gl, "white1x1", "assets/textures/white1x1.jpg");
+
+    light = new Light(gl, camera);
+    Material m = new Material();
+    m.setAmbient(0.3f, 0.3f, 0.3f);
+    m.setDiffuse(0.7f, 0.7f, 0.7f);
+    m.setSpecular(0.7f, 0.7f, 0.7f);
+    light.setMaterial(m);
+
+    flatPlane = new Model[3];
+    flatPlane[0] = makeFlatPlane(gl, getM1(), "assets/shaders/fs_standard_d.txt", textures.get("chequerboard"), null, null);
+    flatPlane[1] = makeFlatPlane(gl, getM2(), "assets/shaders/fs_standard_d.txt", textures.get("cloud"), null, null);
+    flatPlane[2] = makeFlatPlane(gl, getM3(), "assets/shaders/fs_standard_0t.txt", null, null, null);
+  
+    cube = new Model[3];
+    cube[0] = makeCube(gl, Mat4.multiply(Mat4Transform.translate(0,4,0), Mat4Transform.scale(4,4,4)),
+                       "assets/shaders/fs_standard_dse.txt", textures.get("diffuse_container"), textures.get("specular_container"), textures.get("matrix"));
+    cube[1] = makeCube(gl, Mat4.multiply(Mat4Transform.translate(0,4,0), Mat4Transform.scale(2,6,2)),
+                       "assets/shaders/fs_standard_dse.txt", textures.get("diffuse_container"), textures.get("specular_container"), textures.get("matrix"));
+    cube[2] = makeCube(gl, Mat4.multiply(Mat4Transform.translate(0,4,0), Mat4Transform.scale(6,2,6)),
+                       "assets/shaders/fs_standard_dse.txt", textures.get("diffuse_container"), textures.get("specular_container"), textures.get("matrix"));
+  }
+
+  public void render(GL3 gl) {
+    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+    light.setPosition(getLightPosition()); // changing light position each frame
+
+    light.render(gl);
+
+    flatPlane[0].render(gl);
+    flatPlane[1].render(gl);
+    flatPlane[2].render(gl);
+    
+    cube[0].render(gl);
+    cube[1].render(gl);
+    cube[2].render(gl);
+
+
+  }
+  
+
+  // **********************************
+  /* Floor
+   */
+
+  private Model makeFlatPlane(GL3 gl, Mat4 m, String fragmentPath, Texture diffuse, Texture specular, Texture emission) {
+    String name = "floor";
+    Mesh mesh = new Mesh(gl, TwoTriangles.vertices.clone(), TwoTriangles.indices.clone());
+    Mat4 modelMatrix = m;
+    Shader shader = new Shader(gl, "assets/shaders/vs_standard.txt", fragmentPath);
+    Material material = new Material(new Vec3(0.1f, 0.5f, 0.91f), new Vec3(0.1f, 0.5f, 0.91f), new Vec3(0.3f, 0.3f, 0.3f), 4.0f);
+    material.setDiffuseMap(diffuse);
+    material.setSpecularMap(specular);
+    material.setEmissionMap(emission);
+    Renderer renderer = new Renderer();
+    return new Model(name, mesh, modelMatrix, shader, material, renderer, light, camera);
+  }
+
+  // **********************************
+  /* Cube
+   */
+
+  private Mat4 getModelMatrix(int i) {
+    double elapsedTime = getSeconds()-startTime;
+    Mat4 model = new Mat4(1);    
+    float yAngle = (float)(elapsedTime*10*randoms[(i+637)%NUM_RANDOMS]);
+    float multiplier = 12.0f;
+    float x = multiplier*randoms[i%NUM_RANDOMS] - multiplier*0.5f;
+    float y = 0.5f+ (multiplier*0.5f) + multiplier*randoms[(i+137)%NUM_RANDOMS] - multiplier*0.5f;
+    float z = multiplier*randoms[(i+563)%NUM_RANDOMS] - multiplier*0.5f;
+    model = Mat4.multiply(model, Mat4Transform.translate(x,y,z));
+    model = Mat4.multiply(model, Mat4Transform.rotateAroundY(yAngle));
+    return model;
+  }
+
+  private Mat4 getCubeModelMatrix() {
+    Mat4 modelMatrix = new Mat4(1);
+    modelMatrix = Mat4.multiply(Mat4Transform.scale(4f,4f,4f), modelMatrix);
+    return modelMatrix;
+  }
+
+  private Model makeCube(GL3 gl, Mat4 m, String fragmentPath, Texture diffuse, Texture specular, Texture emission) {
+    String name = "cube";
+    Mesh mesh = new Mesh(gl, Cube.vertices.clone(), Cube.indices.clone());
+    Mat4 modelMatrix = m;
+    Shader shader = new Shader(gl, "assets/shaders/vs_standard.txt", fragmentPath);
+    Material material = new Material(new Vec3(1.0f, 0.5f, 0.31f), new Vec3(1.0f, 0.5f, 0.31f), new Vec3(0.5f, 0.5f, 0.5f), 32.0f);
+    material.setDiffuseMap(diffuse);
+    material.setSpecularMap(specular);
+    material.setEmissionMap(emission);
+    Renderer renderer = new Renderer();
+    return new Model(name, mesh, modelMatrix, shader, material, renderer, light, camera);
+  }
+
+  // **********************************
+  /* Light
+   */
+
+    // The light's postion is continually being changed, so needs to be calculated for each frame.
+  private Vec3 getLightPosition() {
+    double elapsedTime = getSeconds()-startTime;
+    float x = 7.0f*(float)(Math.sin(Math.toRadians(elapsedTime*50)));
+    float y = 5.4f;
+    float z = 7.0f*(float)(Math.cos(Math.toRadians(elapsedTime*50)));
+    return new Vec3(x,y,z);
+  }
+
+// ***************************************************
+
+ 
+  private Mat4 getM1() {
+    float size = 16f;
+    Mat4 modelMatrix = new Mat4(1);
+    modelMatrix = Mat4.multiply(Mat4Transform.scale(size,1f,size), modelMatrix);
+    return modelMatrix;
+  }
+  
+  private Mat4 getM2() {
+    float size = 16f;
+    Mat4 modelMatrix = new Mat4(1);
+    modelMatrix = Mat4.multiply(Mat4Transform.scale(size,1f,size), modelMatrix);
+    modelMatrix = Mat4.multiply(Mat4Transform.rotateAroundX(90), modelMatrix);
+    modelMatrix = Mat4.multiply(Mat4Transform.translate(0,size*0.5f,-size*0.5f), modelMatrix);
+    return modelMatrix;
+  }
+
+  private Mat4 getM3() {
+    float size = 16f;
+    Mat4 modelMatrix = new Mat4(1);
+    modelMatrix = Mat4.multiply(Mat4Transform.scale(size,1f,size), modelMatrix);
+    modelMatrix = Mat4.multiply(Mat4Transform.rotateAroundY(90), modelMatrix);
+    modelMatrix = Mat4.multiply(Mat4Transform.rotateAroundZ(-90), modelMatrix);
+    modelMatrix = Mat4.multiply(Mat4Transform.translate(-size*0.5f,size*0.5f,0), modelMatrix);
+    return modelMatrix;
+  }
+
+  // ***************************************************
+  /* TIME
+   */ 
+  
+  private double startTime;
+  
+  private double getSeconds() {
+    return System.currentTimeMillis()/1000.0;
+  }
+  
+    // ***************************************************
+  /* An array of random numbers
+   */ 
+  
+  private int NUM_RANDOMS = 1000;
+  private float[] randoms;
+  
+  private void createRandomNumbers() {
+    randoms = new float[NUM_RANDOMS];
+    for (int i=0; i<NUM_RANDOMS; ++i) {
+      randoms[i] = (float)Math.random();
+    }
+  }
+
+}
